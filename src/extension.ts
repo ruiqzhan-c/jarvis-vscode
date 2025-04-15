dac6dd2ad3b2550fa5585cc3496877b0b3d4b637
Ruiqi Zhang <ruiqzhan@cisco.com>
2025-04-15 15:12:44 +0100

    feat: minimal chat participant with message history

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,57 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+const BASE_PROMPT = "You are Jarvis, a helpful assistant. If I write foo, you respond with bar.";
+
+// define a chat handler
+const handler: vscode.ChatRequestHandler = async (
+	request: vscode.ChatRequest,
+	context: vscode.ChatContext,
+	stream: vscode.ChatResponseStream,
+	token: vscode.CancellationToken
+  ) => {
+	// initialize the prompt
+	let prompt = BASE_PROMPT;
+  
+	// initialize the messages array with the prompt
+	const messages = [vscode.LanguageModelChatMessage.User(prompt)];
+
+	// get all the previous participant messages
+	const previousMessages = context.history.filter(
+		h => h instanceof vscode.ChatResponseTurn
+	);
+
+	// add the previous messages to the messages array
+	previousMessages.forEach(m => {
+		let fullMessage = '';
+		m.response.forEach(r => {
+		  const mdPart = r as vscode.ChatResponseMarkdownPart;
+		  fullMessage += mdPart.value.value;
+		});
+		messages.push(vscode.LanguageModelChatMessage.Assistant(fullMessage));
+	  });
+  
+	// add in the user's message
+	messages.push(vscode.LanguageModelChatMessage.User(request.prompt));
+  
+	// send the request
+	const chatResponse = await request.model.sendRequest(messages, {}, token);
+  
+	// stream the response
+	for await (const fragment of chatResponse.text) {
+	  stream.markdown(fragment);
+	}
+	console.log('responding...');
+	return;
+  };
+
+// create participant
+const tutor = vscode.chat.createChatParticipant('jarvis.jarvis', handler);
+  
+// add icon to participant
+//   tutor.iconPath = vscode.Uri.joinPath(context.extensionUri, 'tutor.jpeg');
+  
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
