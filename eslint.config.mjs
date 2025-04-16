e5df58f53f4c49f0e48c18c0483362d5335b02ec
Ruiqi Zhang <ruiqzhan@cisco.com>
2025-04-16 13:03:52 +0100

    refactor: add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,16 +2,19 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-    eslint.configs.recommended,
-    tseslint.configs.recommendedTypeChecked,
-    tseslint.configs.stylisticTypeChecked,
-    tseslint.configs.eslintRecommended,
-    {
-        languageOptions: {
-            parserOptions: {
-                projectService: true,
-                tsconfigRootDir: import.meta.dirname,
-            },
-        },
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  tseslint.configs.eslintRecommended,
+  {
+    ignores: [
+      "*.mjs",
+    ],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
-  );
+  },
+);
--- src/extension.ts
+++ src/extension.ts
@@ -6,75 +6,75 @@ const CISCO_PROMPT = "You are Jarvis, a helpful assistant. Your job is to give a
 
 // Main chat handler for Jarvis
 const handler: vscode.ChatRequestHandler = async (
-	request: vscode.ChatRequest,
-	context: vscode.ChatContext,
-	stream: vscode.ChatResponseStream,
-	token: vscode.CancellationToken
-  ) => {
+  request: vscode.ChatRequest,
+  context: vscode.ChatContext,
+  stream: vscode.ChatResponseStream,
+  token: vscode.CancellationToken
+) => {
 
-	// Handles case where Jarvis is @ed but no prompt is given
-	if (request.prompt.length === 0) {
-		console.log("no prompt received");
-		stream.markdown("Please enter a prompt.");
-		return;
-	}
+  // Handles case where Jarvis is @ed but no prompt is given
+  if (request.prompt.length === 0) {
+    console.log("no prompt received");
+    stream.markdown("Please enter a prompt.");
+    return;
+  }
 
-	let prompt = BASE_PROMPT;
+  let prompt = BASE_PROMPT;
 
-	// Check if the request is a command and set the prompt accordingly
-	switch (request.command) {
+  // Check if the request is a command and set the prompt accordingly
+  switch (request.command) {
 
-		case "cisco":
-			console.log("command: cisco");
-			prompt = CISCO_PROMPT;
-			break;
+    case "cisco":
+      console.log("command: cisco");
+      prompt = CISCO_PROMPT;
+      break;
 
-		default:
-			console.log("command: none");
-	}
-  
-	// Initialise messages with base prompt
-	const messages = [vscode.LanguageModelChatMessage.User(prompt)];
+    default:
+      console.log("command: none");
+  }
 
-	// Get all previous participant messages
-	const previousMessages = context.history.filter(
-		h => h instanceof vscode.ChatResponseTurn
-	);
+  // Initialise messages with base prompt
+  const messages = [vscode.LanguageModelChatMessage.User(prompt)];
 
-	previousMessages.forEach(m => {
-		let fullMessage = "";
-		m.response.forEach(r => {
-		  const mdPart = r as vscode.ChatResponseMarkdownPart;
-		  fullMessage += mdPart.value.value;
-		});
-		messages.push(vscode.LanguageModelChatMessage.Assistant(fullMessage));
-	  });
-  
-	messages.push(vscode.LanguageModelChatMessage.User(request.prompt));
+  // Get all previous participant messages
+  const previousMessages = context.history.filter(
+    h => h instanceof vscode.ChatResponseTurn
+  );
 
-	const chatResponse = await request.model.sendRequest(messages, {}, token);
+  previousMessages.forEach(m => {
+    let fullMessage = "";
+    m.response.forEach(r => {
+      const mdPart = r as vscode.ChatResponseMarkdownPart;
+      fullMessage += mdPart.value.value;
+    });
+    messages.push(vscode.LanguageModelChatMessage.Assistant(fullMessage));
+  });
 
-	for await (const fragment of chatResponse.text) {
-	  stream.markdown(fragment);
-	}
-  };
+  messages.push(vscode.LanguageModelChatMessage.User(request.prompt));
+
+  const chatResponse = await request.model.sendRequest(messages, {}, token);
+
+  for await (const fragment of chatResponse.text) {
+    stream.markdown(fragment);
+  }
+};
 
 export function activate(context: vscode.ExtensionContext) {
 
-	console.log("extension Jarvis is now active...");
+  console.log("extension Jarvis is now active...");
 
-	// Placeholder command
-	const disposable = vscode.commands.registerCommand("jarvis.hello", () => {
-		vscode.window.showInformationMessage("Jarvis says hi!");
-	});
-	context.subscriptions.push(disposable);
+  // Placeholder command
+  const disposable = vscode.commands.registerCommand("jarvis.hello", () => {
+    vscode.window.showInformationMessage("Jarvis says hi!");
+  });
+  context.subscriptions.push(disposable);
 
-	// Jarvis chat participant
-	const tutor = vscode.chat.createChatParticipant("jarvis.jarvis", handler);
-	tutor.iconPath = vscode.Uri.joinPath(context.extensionUri, "jarvis-icon.webp");
+  // Jarvis chat participant
+  const tutor = vscode.chat.createChatParticipant("jarvis.jarvis", handler);
+  tutor.iconPath = vscode.Uri.joinPath(context.extensionUri, "jarvis-icon.webp");
 
 }
 
 export function deactivate() {
-	console.log("extension Jarvis is now deactivated...");
+  console.log("extension Jarvis is now deactivated...");
 }
